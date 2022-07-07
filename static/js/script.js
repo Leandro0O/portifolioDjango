ced2bb579d7ca9e98e30ba9878aa645856062c03
Leandro Gonçalves <goncalvesleandro68@gmail.com>
2022-07-07 17:17:38 -0300

    Att medias

--- static/js/script.js
+++ static/js/script.js
@@ -8,12 +8,15 @@ window.addEventListener("scroll", function () {
 let menu = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
+
+
 menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navbar.classList.toggle('active');
 };
 
-window.onscroll = () =>{
+
+window.onscroll = () => {
     menu.classList.remove('bx-x');
     navbar.classList.remove('active');
 }
